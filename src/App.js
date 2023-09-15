8e7b8e9786604e4f02bca901e84034cd5a663350
emi-najeeb <muhammadnajeeb@emifusion.com>
2023-09-15 12:50:08 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  };
+  const decrement = () => {
+    dispatch(actions.decrement());
+  };
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+}
+
+export default App;
